7675f5caee322e2c92cc56fa086b373c7f20a755
projectgoav <edm22@hotmail.co.uk>
2015-10-03 16:22:40 +0100

    Updated key bindings and modified the menu

--- kinectApp/kinectApp/kinectApp/Entities/Scenes/ExitGameIntsance.cs
+++ kinectApp/kinectApp/kinectApp/Entities/Scenes/ExitGameIntsance.cs
@@ -19,18 +19,31 @@ namespace kinectApp.Entities.Scenes
 
         public ExitGameIntsance() : base("ExitGameInstance")
         {
-            iMessage = new Label("Bye Bye?", string.Empty, 50, 50, 0);
+            iMessage = new Label("Bye Bye?", string.Empty, 50, 100, 0);
+
+            iYesbtn = new Button("Yes", string.Empty, 50, 135, 0);
+            iNobtn = new Button("Back", string.Empty, 175, 135, 0);
 
             Entities.Add(iMessage);
+            Entities.Add(iYesbtn);
+            Entities.Add(iNobtn);
         }
 
         public override void HandleKeys(InputHelper aInputHelper, ISceneManager aSceneManager)
         {
             //Close/Quit :)
 
-            if(aInputHelper.IsNewPress(Keys.Y) || aInputHelper.IsNewPress(Keys.Enter))
+            if(aInputHelper.IsNewPress(Keys.Q) || aInputHelper.IsNewPress(Keys.Enter) || aInputHelper.IsNewPress(Keys.Y))
             {
                 Game1.ForceClose();
+                return;
+            }
+
+            //We don't want to close quit - return to previous screen
+            if (aInputHelper.IsNewPress(Keys.N) || aInputHelper.IsNewPress(Keys.Back))
+            {
+                aSceneManager.HideOverlay();
+                return;
             }
         }
     }
--- kinectApp/kinectApp/kinectApp/Entities/Scenes/Menu.cs
+++ kinectApp/kinectApp/kinectApp/Entities/Scenes/Menu.cs
@@ -20,17 +20,21 @@ namespace kinectApp.Entities.Scenes
         private IEntity _OptionB;
         private IEntity _Quit;
         private IEntity _Play;
+        private IEntity iLabel;
 
         public Menu() : base("Menu")
         {
-            _Quit = new Button("Quit", string.Empty, 20, 50, 0);
-            _Play = new Button("Play", string.Empty, 20, 80, 0);
+            _Play = new Button("Play", string.Empty, 20, 50, 0);
+            _Quit = new Button("Quit", string.Empty, 20, 85, 0);
+
+            iLabel = new Label("Dynamic Dorks :)", string.Empty, 500, 0, 0);
 
             _OptionA = null;
             _OptionB = null;
 
             Entities.Add(_Quit);
             Entities.Add(_Play);
+            Entities.Add(iLabel);
         }
 
         public override void HandleKeys(InputHelper aInputHelper, ISceneManager aSceneManager)
@@ -38,7 +42,7 @@ namespace kinectApp.Entities.Scenes
             //Does the user want to quit?
             if (aInputHelper.IsNewPress(Keys.Q) || aInputHelper.IsNewPress(Keys.Escape))
             {
-                aSceneManager.SetScene(new ExitGameIntsance());
+                aSceneManager.ShowOverlay(new ExitGameIntsance());
                 return;
             }
 
