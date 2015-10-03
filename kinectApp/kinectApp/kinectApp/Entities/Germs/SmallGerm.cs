a7e1b1ef75dc3111c5252bd6c52b928bd486cd39
dylan stevenson <stevensondylan13@googlemail.com>
2015-10-03 02:08:08 +0100

    "added some logi"

--- kinectApp/kinectApp/kinectApp/Entities/Germs/SmallGerm.cs
+++ kinectApp/kinectApp/kinectApp/Entities/Germs/SmallGerm.cs
@@ -31,6 +31,8 @@ namespace kinectApp.Entities.Germs
 
         public override void Update(GameTime aGameTime)
         {
+            this.PosX += 2;
+            this.PosY += 10;
             //throw new NotImplementedException();
         }
     }
