30317b3a209a501ccb48c5f7c53593e62811a133
projectgoav <edm22@hotmail.co.uk>
2015-10-03 17:39:38 +0100

    Pause Screen implemented

--- kinectApp/kinectApp/kinectApp/Entities/Scenes/Pause.cs
+++ kinectApp/kinectApp/kinectApp/Entities/Scenes/Pause.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework.Input;
+
 using kinectApp.Utilities;
+using kinectApp.Entities.UI;
 
 namespace kinectApp.Entities.Scenes
 {
@@ -12,14 +16,22 @@ namespace kinectApp.Entities.Scenes
     */
     public class Pause : Scene
     {
+        private IEntity iLabel;
+
         public Pause() : base("Pause")
         {
+            iLabel = new Label("Paused...", string.Empty, 250, 250, 0);
 
+            Entities.Add(iLabel);
         }
 
+        //Pause Screen is dismissed with Space!
         public override void HandleKeys(InputHelper aInputHelper, ISceneManager aSceneManager)
         {
-            throw new NotImplementedException();
+            if (aInputHelper.IsNewPress(Keys.Space))
+            {
+                aSceneManager.HideOverlay();
+            }
         }
     }
 }
