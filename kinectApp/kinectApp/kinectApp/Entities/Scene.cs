718072152471b10cc534983dd6229bf784cd99f6
projectgoav <edm22@hotmail.co.uk>
2015-10-03 03:25:59 +0100

    Added code for a 'scene' which should allow changes in future for switching levels / windows.

---
+++ kinectApp/kinectApp/kinectApp/Entities/Scene.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kinectApp.Entities
+{
+    public interface IScene
+    {
+        List<IEntity> Entities { get; }
+        string Name { get; }
+
+        void Load(EntityManager aManager);
+    }
+
+
+
+    /*
+        Defines a list of entities for the Entity Manager to load in when required.
+        Would define parts of game - Menu / Game / HighScores / GameOver etc
+    */
+    public class Scene
+    {
+        private string _name;
+        private List<IEntity> _entites;
+
+        public Scene(string aName)
+        {
+            _name = aName;
+            _entites = new List<IEntity>();
+        }
+
+        public List<IEntity> Entities { get { return _entites; } }
+        public string Name { get { return _name; } }
+    }
+}
