2a29b939cb68bf51ff495a524de1ae555c32b39d
projectgoav <edm22@hotmail.co.uk>
2015-10-03 03:31:41 +0100

    Added outlines for the main scenes in the gane

---
+++ kinectApp/kinectApp/kinectApp/Entities/Scenes/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kinectApp.Entities.Scenes
+{
+    /*
+        Game Main Menu
+    */
+    public class Menu : Scene
+    {
+        public Menu() : base("Menu")
+        {
+
+        }
+    }
+}
---
+++ kinectApp/kinectApp/kinectApp/Entities/Scenes/Pause.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kinectApp.Entities.Scenes
+{
+    /*
+        Pause Screen of the Game
+    */
+    public class Pause : Scene
+    {
+        public Pause() : base("Pause")
+        {
+
+        }
+
+    }
+}
